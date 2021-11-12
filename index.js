ccabfdf98724c4db342e318d09d18ec2a40ed6d7
Parth Gupta <parthg635@gmail.com>
2021-11-12 16:07:02 +0000

    commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const product = require("./api/product");
+const dt = require("./main")
+dt.getD("SOL")
+app.use(express.json({ extended: false }));
+
+app.use("/api/product", product);
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
