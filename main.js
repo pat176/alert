2acd9f5ef22e2a804352b4d0371991fe339174db
Parth Gupta <parthg635@gmail.com>
2021-11-12 14:08:45 +0000

    first

---
+++ main.js
@@ -0,0 +1,61 @@
+const { sendMail } = require('./reportSender');
+const https = require('https');
+const ta = require('technicalindicators')
+const getD = (cryp) => {setInterval(() => {
+  (async () => {
+    https.get(`https://min-api.cryptocompare.com/data/v2/histominute?fsym=${cryp}&tsym=USD&limit=60&api_key=b8a4ce0dc14c286b06e7c6e710649a82aa5bf1a73f7080dc026e988eb91ff2f9`, (resp) => {
+      let high = [];
+      let low = [];
+      let close = [];
+      // A chunk of data has been received.
+      resp.on('data', (chunk) => {
+        // console.log(JSON.parse(chunk).Data.Data)
+        for (let index = 0; index < JSON.parse(chunk).Data.Data.length; index++) {
+          high.push(JSON.parse(chunk).Data.Data[index].high)
+          low.push(JSON.parse(chunk).Data.Data[index].low)
+          close.push(JSON.parse(chunk).Data.Data[index].close)
+          
+        }
+      });
+      
+      // The whole response has been received. Print out the result.
+      resp.on('end', () => {
+        // console.log(high);
+        const stoch = ta.Stochastic.calculate({
+          high: high,
+          low: low,
+          close: close,
+          period: 5,
+          signalPeriod: 3
+        })
+        if (stoch[stoch.length - 1].k > stoch[stoch.length - 1].d && stoch[stoch.length - 1].k <= 20 && stoch[stoch.length - 2].k < stoch[stoch.length - 2].d) sendMail("Buy "+cryp, "Buy")
+        else if (stoch[stoch.length - 1].k < stoch[stoch.length - 1].d && stoch[stoch.length - 1].k >= 80  && stoch[stoch.length - 2].k > stoch[stoch.length - 2].d) sendMail("Sell "+cryp, "Sell")
+        // console.log(stoch)
+      });
+      
+    }).on("error", (err) => {
+      // console.log("Error: " + err.message);
+    })
+  })()
+}, 1000);}
+getD("BTC")
+getD("SOL")
+// const getSolData = async () => {
+//   https.get('https://min-api.cryptocompare.com/data/v2/histominute?fsym=SOL&tsym=USD&limit=10&api_key=b8a4ce0dc14c286b06e7c6e710649a82aa5bf1a73f7080dc026e988eb91ff2f9', (resp) => {
+  
+//     // A chunk of data has been received.
+//     resp.on('data', (chunk) => {
+//       dataSOL += chunk;
+//     });
+  
+//     // The whole response has been received. Print out the result.
+//     resp.on('end', () => {
+      console.log(JSON.parse(dataSOL));
+//     });
+  
+//   }).on("error", (err) => {
+    console.log("Error: " + err.message);
+//   });
+  
+// }
+// getSolData()
---
+++ reportSender.js
@@ -0,0 +1,30 @@
+const nodemailer = require('nodemailer');
+const defaultMailingList = "parthgupta12102004@gmail.com";
+const senderEmail = "parth.eng1210@gmail.com";
+const senderPassword = "1210@parth"; // gmail app password
+module.exports = {
+  sendMail: async (subject, text, to = defaultMailingList) => {
+    try {
+      const transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+          user: senderEmail,
+          pass: senderPassword,
+        },
+      });
+
+      const message = {
+        from: senderEmail,
+        to,
+        subject,
+        text: subject,
+        html: text,
+      };
+
+      transporter.sendMail(message, () => { console.log("here") });
+    } catch (e) {
+      // handle errors here
+    }
+  },
+};
+//b8a4ce0dc14c286b06e7c6e710649a82aa5bf1a73f7080dc026e988eb91ff2f9
